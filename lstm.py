5988aea4523c8c9cb1e39f56a27df95043a4390f
Jason Shin <visualbbasic@gmail.com>
2018-01-07 11:32:24 +1100

    wip lstm

--- lstm.py
+++ lstm.py
@@ -45,6 +45,35 @@ class RecurrentNeuralNetwork:
         # input gate
         self.ai = np.zeros((rl + 1, ys))
 
+        # cell state
+        self.ac = np.zeros((rl + 1, ys))
+
+        # output gate
+        self.ao = np.zeros((rl + 1, ys))
+
+        # array of expected output values
+        self.eo = np.vstack((np.zeros(eo.shape[0]), eo.T))
+
+        # declare LSTM cell (input, output, amount of recurrence, Learning rate)
+        self.LSTM = LSTM(xs, ys, rl, lr)
+
+    # activation function. simple nonlinearity, convert nums into probabilities between 0 and 1
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    # the derivative of the sigmoid function.
+    def dsigmoid(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
+
+    def forwardProp(self):
+        for i in range(1, self.rl + 1):
+            self.LSTM.x = np.hstack((self.ha[i - 1], self.x))
+            cs, hs, f, c, o = self.LSTM.forwardProp()
+            # store computed cell state
+            self.ca[i] = cs
+            self.ha[i] = hs
+            self.af[i] = f
+            self.ai[i] = inp
 
 
 
