84d32b2069ae69d7bd85f2f0172f69087fba7116
Jason Shin <visualbbasic@gmail.com>
2018-01-21 12:44:23 +1100

    adding backprop

--- backprops/backprop.py
+++ backprops/backprop.py
@@ -26,14 +26,48 @@ def forward_propagate(network, row):
     return inputs
 
 
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+
+# Backpopagate error and store in neurons
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network) - 1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+
 def main():
+    # Forward prop example
     network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
                [{'weights': [0.2550690257394217, 0.49543508709194095]},
                 {'weights': [0.4494910647887381, 0.651592972722763]}]]
     row = [1, 0, None]
-    # Forward prop example
     output = forward_propagate(network, row)
-    print('checking stuff!', output)
+
+    # test backpropagation of error
+    network = [
+        [{'output': 0.7105668883115941, 'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+        [{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095]},
+         {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763]}]]
+    expected = [0, 1]
+    backward_propagate_error(network, expected)
+    print('Finished backpropagating errors')
+    for layer in network:
+        print(layer)
 
 
 if __name__ == '__main__':
